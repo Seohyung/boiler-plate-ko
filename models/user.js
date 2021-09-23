0c4d41d2b1b5e00d1da72332efd65a36c48abbbf
seohyung <seohyung.whang@gmail.com>
2021-09-23 18:38:27 +0900

    restart: changing mongodb to mysql

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        email: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        provider: {
+          type: Sequelize.STRING(10),
+          allowNull: false,
+          defaultValue: 'local',
+        },
+        snsID: {
+          type: Sequelize.STRING(30),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: true,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+      }
+    );
+  }
+  static associate(db) {}
+};
